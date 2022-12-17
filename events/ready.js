59cb4d11e4a94821bba67e1daa49e87d770c8cbb
meesam4687 <meesam4687@gmail.com>
2022-12-17 11:28:51 +0530

    Added Status Message

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
+        client.user.setPresence({ activities: [{ name: 'Finding Lurkers' }], status: 'online' }),
         console.log(`Running as ${client.user.username}`);
     },
 };
